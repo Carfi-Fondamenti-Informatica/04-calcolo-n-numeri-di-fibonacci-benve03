15b6cb9a922b2b8b285c7772fdbeee9f0fe406c5
benve03 <117894307+benve03@users.noreply.github.com>
2022-11-24 14:42:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, a=1, b=1, c=0;
+    cout <<"inserisci primo valore della sequenza"<< endl;
+    cin >>n;
+    if (n>=2){
+        cout << a << endl;
+        cout << b << endl;
+        for (int i = 0; i+2 < n; i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout <<c<< endl;
+        }
+    }
+    else {
+        cout <<"errore"<< endl;
+    }
+    return 0;
 }
+
